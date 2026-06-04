db4ccbb5c7597805b1d09d2d348ea3de40e12931
DuvanchoU <duvanfuribe@gmail.com>
2026-06-04 11:21:54 -0500

    feat: migrar configuración BD de MySQL a PostgreSQL, se actualiza requirements.txt

--- config/settings.py
+++ config/settings.py
@@ -106,15 +106,14 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.getenv('DB_NAME', 'bd_orden_rae_django'),
-        'USER': os.getenv('DB_USER', 'root'), 
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME', ''),
+        'USER': os.getenv('DB_USER', 'postgres'), 
         'PASSWORD': os.getenv('DB_PASSWORD', ''), 
         'HOST': os.getenv('DB_HOST', 'localhost'),
-        'PORT': os.getenv('DB_PORT', '3306'),
+        'PORT': os.getenv('DB_PORT', '5432'),
         'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-            'charset': 'utf8mb4', 
+            'client_encoding': 'UTF8',
         },
     }
 }
